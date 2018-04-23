aa9976d46a42866d0337f9aa9b451e96f2491c59
liyixingustc <liyixingustc@gmail.com>
2018-04-22 22:27:20 -0400

    FE-BE-ML intergration

--- app.py
+++ app.py
@@ -4,22 +4,21 @@ import logging
 from flask import Flask, request, jsonify, render_template, redirect, flash
 from flask_cors import CORS
 from datetime import datetime
-from model import lr_model, multi_model
+from model import ml_predict
 from flask_wtf import Form
 from wtforms.fields import IntegerField, SubmitField
 from wtforms import validators, ValidationError
 import csv
 import numpy as np
 from flask import send_file
+import pandas as pd
+import json
 
 class MyForm(Form):
-    zipcode = IntegerField('zipcode', [validators.DataRequired(), 
-                                       validators.NumberRange(
-                                           min = 10000, 
-                                           max = 99999
-                                       )])
     numBed = IntegerField('numBed')
     numBath = IntegerField('numBath')
+    zipcode = IntegerField('zipcode', [validators.Optional()])
+    sqft = IntegerField('Square foot')
     submit = SubmitField('Search my next home!')
 
 # define the app
@@ -41,33 +40,50 @@ def index():
     form = MyForm()
     if request.method == 'POST' and form.validate():
         data = request.form
-        zipcode = int(form.data['zipcode'])
-        results = multi_model.multi_model(zipcode)
-        print (results)
-        bedno   = int(form.data['numBed'])
-        bathno  = int(form.data['numBath'])
+        numBed = int(form.data['numBed'])
+        numBath = int(form.data['numBath'])
+        sqft = int(form.data['sqft'])
+        try:
+            zipcode = int(form.data['zipcode'])
+            with open('data/CityLatLong.csv', 'r') as csvfile:
+                csvReader = csv.reader(csvfile)
+                flag = 0
+                for row in csvReader:
+                    if(row[4]==str(zipcode)):
+                        latitude = float(row[0])
+                        longitude = float(row[1])
+                        city = row[2]
+                        state = row[3]
+                        flag = 1
+                        break
+        except:
+            flag = 0
+
         inputrows = []
-        with open('data/properties_0419.csv', 'r') as csvfile:
+        with open('data/properties_0421.csv', 'r') as csvfile:
             csvReader = csv.reader(csvfile)
             i = 0
             for row in csvReader:
-            	i = i + 1
-            	if row[8] == str(bedno) and row[9] == str(bathno):
+                i = i + 1
+                if row[8] == str(numBed) and row[9] == str(numBath):
                     inputrows.append([i, float(row[6]), float(row[1]), float(row[2])])
+
+        if flag == 0:
+            ml_results = json.dumps("Invalid zipcode or zipcode not support yet! Please come again later!")
+            ml_results = ml_results[1:-1]
+            return render_template('index.html', form = form, ml_results = ml_results, info = inputrows, fcn1 = "visualData(pRange)")
+        else:
+            results = ml_predict.ml_predict(zipcode, latitude, longitude, city, state, numBed, numBath, sqft)
+            results = results[1:-1]
+            ml_results, distance = results.split(",")
+            ml_results = float(ml_results)
+            distance = float(distance)
+            ml_results = '{0:.2f}'.format(ml_results)
+            distance = '{0:.2f}'.format(distance)
+            ml_results = "Price prediction: $" + ml_results
+            distance = "Distance to nearest mall: " + distance + " km"
                     
-        print(len(inputrows))
-        '''
-        inputstring = ["id", "price", "latitude", "longitude"]
-        with open('price_range.csv', 'w') as csvfile:
-            csvWriter = csv.writer(csvfile)
-            csvWriter.writerow(inputstring)
-            for row in inputrows:
-                temp = row
-                csvWriter.writerow(temp)
-        '''
-
-
-        return render_template('index.html', form = form, ml_results = results, info = inputrows, fcn1 = "visualData(pRange)")
+            return render_template('index.html', form = form, ml_results = ml_results, distance = distance, info = inputrows, fcn1 = "visualData(pRange)")
     return render_template('index.html', form = form)
 
 
